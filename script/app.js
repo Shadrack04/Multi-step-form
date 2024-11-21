a1c81ac7e787bf31254df05005294be0b49203c9
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2024-11-21 06:15:06 +0100

    Add validation to the step 1 form

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-import { isEmptyInput, isInvalidEmail } from "./validate.js";
+import { isInputInvalid } from "./validate.js";
 
 const contentContainer = document.querySelector(".content");
 const steps = contentContainer.querySelectorAll("[data-step]");
@@ -10,13 +10,21 @@ if (currentStep < 0) {
   showStep();
 }
 
+let errorArray = [];
 contentContainer.addEventListener("click", (e) => {
+  let incrementor;
   if (e.target.matches("[data-next]")) {
-    currentStep += 1;
+    //currentStep += 1;
+    incrementor = 1;
   } else if (e.target.matches("[data-prev]")) {
-    currentStep -= 1;
+    //currentStep -= 1;
+    incrementor = -1;
+  } else return;
+  errorArray = isInputInvalid();
+  if (errorArray.length === 0) {
+    currentStep += incrementor;
+    showStep();
   }
-  showStep();
 });
 
 function showStep() {
--- script/validate.js
+++ script/validate.js
@@ -2,6 +2,17 @@ const name = document.getElementById("name");
 const email = document.getElementById("email");
 const phoneNumber = document.getElementById("phone");
 const emailError = document.getElementById("email-error");
+const nameError = document.getElementById("name-error");
+const phoneError = document.getElementById("phone-error");
+
+export const isInvalidEmail = () => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(email.value)) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
 export const isEmptyInput = () => {
   if (name.value === "" || email.value === "" || phoneNumber.value === "") {
@@ -11,12 +22,24 @@ export const isEmptyInput = () => {
   }
 };
 
-export const isInvalidEmail = () => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (emailRegex.test(email.value)) {
-    emailError.textContent = "Enter a valid Email";
-    return false;
-  } else {
-    return true;
+function clearInputError() {
+  nameError.textContent = "";
+  emailError.textContent = "";
+  phoneError.textContent = "";
+}
+
+export function isInputInvalid() {
+  clearInputError();
+  let error = [];
+  if (name.value === "") {
+    nameError.textContent = "Enter your name";
+    error.push(true);
+  } else if (email.value === "" || isInvalidEmail()) {
+    emailError.textContent = "Enter a valid email";
+    error.push(true);
+  } else if (phoneNumber.value === "") {
+    phoneError.textContent = "Enter your phone number";
+    error.push(true);
   }
-};
+  return error;
+}
